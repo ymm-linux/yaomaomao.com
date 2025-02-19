1a1cf4e68b0e6d787bc67482ef57ef0d075f23a5
yaowei <kiokyw@163.com>
2025-02-19 15:54:34 +0800

    项目结构，运行示例

---
+++ src/main/java/com/yaomaomao/YaomaomaoApplication.java
@@ -0,0 +1,11 @@
+package com.yaomaomao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaomaomaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YaomaomaoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yaomaomao/YaomaomaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaomaomao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaomaomaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
